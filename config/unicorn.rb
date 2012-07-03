eea2d111a5732db75585359d9ee73577e90b31b5
Joe Meirow <joe.meirow@gmail.com>
2012-07-02 20:30:42 -0400

    more config..

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,7 @@
-root = "/home/deployer/apps/grpbldr/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+working_directory "/home/deployer/apps/grpbldr/current"
+pid "/home/deployer/apps/grpbldr/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deployer/apps/grpbldr/current/log/unicorn.log"
+stdout_path "/home/deployer/apps/grpbldr/current/log/unicorn.log"
 
 listen "/tmp/unicorn.grpbldr.sock"
 worker_processes 2
