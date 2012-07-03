97348b2d58fa42d54d4ba1067d0d3b09a304156c
Joe Meirow <joe.meirow@gmail.com>
2012-07-02 22:22:30 -0400

    remove iron worker stuff.

--- lib/services/iron_worker_config.rb
+++
@@ -1,8 +0,0 @@
-module IronWorkerConfig
-
-    IronWorker.configure do |config|     
-      config.token = ENV['IRON_WORKER_TOKEN']
-      config.project_id = ENV['IRON_WORKER_PROJECT_ID']
-    end
-
-end
