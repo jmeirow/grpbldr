c73327935913963b08345ea582594e5b410a08d0
Joe Meirow <joe.meirow@gmail.com>
2012-07-04 15:41:34 -0400

    modified application and environment configs

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module Rolemaster
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.11'
     
     config.active_record.whitelist_attributes = true
 
