5ca11a9f1398e0dffd333bd1b28c89b66e9d4d36
Joe Meirow <joe.meirow@gmail.com>
2012-07-20 17:10:52 -0400

    typo in migration

--- db/migrate/20120717112224_update_email_set_to_as_text_and_add_cc_bcc.rb
+++ db/migrate/20120717112224_update_email_set_to_as_text_and_add_cc_bcc.rb
@@ -2,6 +2,6 @@ class UpdateEmailSetToAsTextAndAddCcBcc < ActiveRecord::Migration
   def change
   	add_column :emails, :cc, :text
   	add_column :emails, :bcc, :text
-  	change_column :emailss, :to, :text
+  	change_column :emails, :to, :text
   end
 end
