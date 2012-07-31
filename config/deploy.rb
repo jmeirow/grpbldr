535bf6a16f3c40e2438560aeea2024624967e26b
Joe Meirow <joe.meirow@gmail.com>
2012-07-30 22:12:00 -0400

    fix git user name

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:ryanb/#{application}.git"
+set :repository, "git@github.com:jmeirow/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
