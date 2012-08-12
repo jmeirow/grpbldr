abeadec39c9f9b8e0b470014c3bc649efb7f764e
Joe Meirow <joe.meirow@gmail.com>
2012-08-11 22:06:34 -0400

    mail can now send plain text message (relay)

--- app/mailers/distribution_list_mailer.rb
+++ app/mailers/distribution_list_mailer.rb
@@ -2,23 +2,27 @@
 
 class DistributionListMailer < ActionMailer::Base
   def relay(id, message)
+
+
+   
   	email = Email.find(id)
-	@body = email.body
+  	 
+	  @body = message.text_part.body.raw_source
+
 	
-	content_type = "text/html"
+	content_type = "text/plain"
 	 
-	message.attachments.each do |attachment|
-		attachments[attachment.filename] = attachment.read
-		content_type = "multipart/mixed"
-		attachments[attachment.filename].content_type = attachment.content_type
-		attachments[attachment.filename].content_disposition = attachment.content_disposition
-		attachments[attachment.filename].content_transfer_encoding = attachment.content_transfer_encoding
-	end
+	# message.attachments.each do |attachment|
+	# 	attachments[attachment.filename] = attachment.read
+	# 	content_type = "multipart/mixed"
+	# 	attachments[attachment.filename].content_type = attachment.content_type
+	# 	attachments[attachment.filename].content_disposition = attachment.content_disposition
+	# 	attachments[attachment.filename].content_transfer_encoding = attachment.content_transfer_encoding
+	# end
 
 	mail(	:from => email.from,
 			:to => email.to, 
 			:subject => email.subject, 
-			:content_transfer_encoding => "quoted-printable", 
 			:content_type => content_type)  
 
 		
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,8 @@ Rolemaster::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :smtp
 
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
