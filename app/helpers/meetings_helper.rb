681dd6bffc6116a8dc26a4735ce1458b5582f307
Joe Meirow <joe.meirow@gmail.com>
2012-09-28 22:41:28 -0400

    new branch for time parser

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,2 +1,10 @@
+
 module MeetingsHelper
+  def meeting_type_info_js
+    result = "var meeting_types = {};\n"
+    @meeting_types.each do |type|
+      result = result + "  meeting_types[" + type.id.to_s + "] = [  " +   ("%02d" % type.hour) +   " , " + ("%02d" % type.minute)  + " ," +  type.am_pm + " ];\n"
+    end
+    result
+  end
 end
